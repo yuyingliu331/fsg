8d02398366978cc922fb341f22f0b954f422446d
Becca Clipper <bclip13@gmail.com>
2016-09-12 18:14:04 -0400

    added products model

---
+++ server/db/models/products.js
@@ -0,0 +1,50 @@
+'use strict'
+var Sequelize = require('sequelize');
+
+var db = require('../_db');
+
+module.exports = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  category: {
+    type: Sequelize.ARRAY,
+    set: function (value) {
+      var arrayOfCategories;
+      if (typeof value === 'string') {
+          arrayOfCategories = value.split(',').map(function (s) {
+              return s.trim();
+          });
+          this.setDataValue('tags', arrayOfCategories);
+      } else {
+          this.setDataValue('tags', value);
+      }
+    }
+  },
+  photo: {
+    type: Sequelize.STRING
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  featured: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
+},{
+  getterMethods: {
+    preview: function() { return this.description.split(0,26)}
+  }
+  //virtual to put price into monetary form?
+}
+)
