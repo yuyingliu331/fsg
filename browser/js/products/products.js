a7fb367bc1345a78548a03daef2245a4aca96b96
Dielentheis <elizabeth.dielentheis@gmail.com>
2016-09-13 18:28:06 -0400

    products and single product view working, but ugly

--- browser/js/products/products.js
+++ browser/js/products/products.js
@@ -1,7 +1,41 @@
 app.config(function($stateProvider) {
 	$stateProvider.state('products', {
 		url: '/products',
-		templateUrl: 'js/products/products.html'
-		// add controller??
-	})
-})
+		templateUrl: 'js/products/products.html',
+		controller: 'ProductsCtrl'
+	});
+});
+
+app.factory('ProductsFactory', function($http) {
+	var returnObj = {};
+
+	returnObj.fetchAll = function() {
+		return $http.get('/api/products')
+		.then(function(products) {
+			return products.data;
+		});
+	}
+
+	returnObj.addToCart = function(product) {
+		$http.post('/api/cart/' /* + USER ID????????*/)
+		.then(function(product) {
+			alert('it was added to cart in DB');
+		});
+	}
+
+	return returnObj;
+});
+
+app.controller('ProductsCtrl', function($scope, ProductsFactory) {
+	ProductsFactory.fetchAll()
+	.then(function(products) {
+		$scope.products = products;
+	});
+
+	$scope.addToCart = function(product) {
+		ProductsFactory.addToCart(product)
+		.then(function(product) {
+			alert('you added it.');
+		});
+	}
+});
