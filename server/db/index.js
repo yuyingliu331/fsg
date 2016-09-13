f169ed5e203245e4891012b1208124aec7b72d37
yuyingliu331 <liuyuying90@live.cn>
2016-09-13 10:54:21 -0400

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -4,9 +4,11 @@ module.exports = db;
 
 // eslint-disable-next-line no-unused-vars
 var User = require('./models/user');
-var Orders = require('./models/order');
+var Order = require('./models/order');
 var Cart = require('./models/cart');
 var Payment = require('./models/payment');
+var Product = require('./models/products');
+var Review = require('./models/reviews');
 
 // if we had more models, we could associate them in this file
 // e.g. User.hasMany(Reports)
@@ -15,4 +17,10 @@ Order.belongsTo(User);
 Cart.belongsTo(User);
 Cart.hasMany(Products);
 Payment.belongsTo(Order);
+Review.belongsTo(User);
+Review.belongsTo(Product);
+
+
+
+
 
