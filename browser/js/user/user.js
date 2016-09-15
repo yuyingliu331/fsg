77f1c2af5f0135e1e4e73cec0bdbfa49ef552757
yuyingliu331 <liuyuying90@live.cn>
2016-09-15 10:14:55 -0400

    Review Feature added

--- browser/js/user/user.js
+++ browser/js/user/user.js
@@ -10,11 +10,13 @@ app.config(function($stateProvider) {
 // app.factory('UserFactory', function($http) {
 // 	var returnObj = {};
 // 	returnObj.findUser = function(userId) {
-// 		$http.get('/api/users/' + userID)
+// 		 $http.get('/api/users/' + userId)
 // 		.then(function(user) {
 // 			return user.data;
 // 		})
 // 	}
+// 	return returnObj;
+// })
 // 	return returnObj;
 	// TODO:
  	// 2) inject UserFactory to productController
