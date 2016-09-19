95a5a3513fb9a5a4abff934d371b43c12a6c4826
Becca Clipper <bclip13@gmail.com>
2016-09-19 17:18:52 -0400

    Checkout Phase One (#64)
    * working on stripe EOD Fri
    
    * stripe button active but not communicating with backend, server now https
    
    * linter clean up

--- browser/js/checkout/checkout.js
+++ browser/js/checkout/checkout.js
@@ -1,7 +1,17 @@
 app.config(function($stateProvider) {
 	$stateProvider.state('checkout', {
 		url: '/checkout',
-		templateUrl: 'js/checkout/checkout.html'
-		// add controller??
+		templateUrl: 'js/checkout/checkout.html',
+    controller: function($scope) {
+          // Stripe publisher key
+      //$window.Stripe.setPublishableKey('pk_test_S0oFek96V9yeMMQdI3aE0A65');
+      $scope.stripeCallback = function (code, result){
+        if (result.error) {
+          alert('it failed! error: ' + result.error.message);
+        } else {
+          alert('sucess! token: ' + result.id);
+        }
+      }
+    }
 	})
 })
