824924a31c87e0772fbfdffae5190580c04687d3
Sungwon Ma <sma2@swarthmore.edu>
2016-09-20 11:46:52 -0400

    catching heroku bug

--- server/env/production.js
+++ server/env/production.js
@@ -5,7 +5,6 @@
     applications environment, along with NODE_ENV=production
 
  */
-console.log('=====\nDATABASE_URL IS:', process.env.DATABASE_URL)
 module.exports = {
     DATABASE_URI: process.env.DATABASE_URL,
     SESSION_SECRET: process.env.SESSION_SECRET,
