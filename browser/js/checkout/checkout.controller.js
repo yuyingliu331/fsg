c47466fcfac44d5ae2fedd79f15207aaec5e6e84
Dielentheis <elizabeth.dielentheis@gmail.com>
2016-09-20 18:38:06 -0400

    views stuff

--- browser/js/checkout/checkout.controller.js
+++ browser/js/checkout/checkout.controller.js
@@ -1,13 +1,26 @@
-app.controller('checkoutCtrl', function($scope, checkoutFactory, $state, $log) {
+app.controller('CheckoutCtrl', function($scope, CartFactory, CheckoutFactory, $state, $log) {
+  var orderObj = {};
+
+  CartFactory.getCart()
+  .then(function(cart) {
+    $scope.cart = cart;
+  });
+
+  $scope.createOrderObj = function() {
+    orderObj = {
+      address:  $scope.address_line2 ? $scope.address_line1 + ', ' + $scope.address_line2 + ', ' + $scope.address_city + ', ' + $scope.address_state + ', ' + $scope.address_zip : $scope.address_line1 + ', ' + $scope.address_city + ', ' + $scope.address_state + ', ' + $scope.address_zip,
+      status: 'processing'
+    };
+  }
 
   $scope.stripeCallback = function (code, result){
     if (result.error) {
       $state.go('paymentDeclined');
     } else {
-      checkoutFactory.processCharge(result)
+      CheckoutFactory.processCharge(result)
       .then(function(res){
         if (res === 200){
-           checkoutFactory.logOrder($scope.orderObj)
+           CheckoutFactory.logOrder(orderObj)
            .catch($log.error)
           $state.go('orderConfirmation');
         } else if (res === 400){
@@ -16,13 +29,5 @@ app.controller('checkoutCtrl', function($scope, checkoutFactory, $state, $log) {
       })
       .catch($log.error)
     }
-  }
-
-  $scope.createOrderObj = function(){
-    $scope.orderObj = {
-      address: $scope.address_line1 + ', ' + $scope.address_line2 + ', ' + $scope.address_city + ', ' + $scope.address_state + ', ' + $scope.address_zip,
-      status: 'processing'
-    }
-  }
-
-})
+  };
+});
--- browser/js/checkout/checkout.js
+++ browser/js/checkout/checkout.js
@@ -2,6 +2,6 @@ app.config(function($stateProvider) {
 	$stateProvider.state('checkout', {
 		url: '/checkout',
 		templateUrl: 'js/checkout/checkout.html',
-    controller: 'checkoutCtrl'
+    controller: 'CheckoutCtrl'
   })
 })
